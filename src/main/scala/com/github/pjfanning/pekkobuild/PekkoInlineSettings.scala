8e6b57653a6b8206dd88f036cb7b44fcb9978920
Matthew de Detrich <matthew.dedetrich@aiven.io>
2024-01-14 12:39:25 +0100

    Add PekkoInlinePlugin (#2)
    * Add PekkoInlinePlugin
    
    * Make pekkoCoreProject its own general setting

---
+++ src/main/scala/com/github/pjfanning/pekkobuild/PekkoInlineSettings.scala
@@ -0,0 +1,8 @@
+package com.github.pjfanning.pekkobuild
+
+import sbt.{SettingKey, settingKey}
+
+trait PekkoInlineSettings {
+  lazy val pekkoInlineEnabled: SettingKey[Boolean] = settingKey("Whether to enable the Scala 2 inliner for Pekko modules." +
+    "Defaults to pekko.no.inline property")
+}
