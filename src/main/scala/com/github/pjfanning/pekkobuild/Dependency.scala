4000a2c5b7ab741a272f9fe91269f1fe3f5d1098
PJ Fanning <pjfanning@users.noreply.github.com>
2024-03-08 19:58:10 +0100

    Update Dependency.scala

--- src/main/scala/com/github/pjfanning/pekkobuild/Dependency.scala
+++ src/main/scala/com/github/pjfanning/pekkobuild/Dependency.scala
@@ -14,8 +14,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
- package com.github.pjfanning.pekkobuild
+
+package com.github.pjfanning.pekkobuild
 
 import sbt.VersionNumber
 
