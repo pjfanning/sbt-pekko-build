648b46eb2ee0b4df6585828c92e9c7d09fb9cde8
Alexey Venderov <avenderov@gradle.com>
2024-04-18 23:56:24 +0200

    Add an auto plugin that configures automatic Build Scan publishing to Apache Develocity server

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Copies 2 features from different Pekko repos that are useful to share around our
 * Mima builds on sbt-mima-plugin and provides support for looking up exclude files in `mima-filters` directories.
     - see https://github.com/apache/incubator-pekko/blob/main/CONTRIBUTING.md#binary-compatibility
  
-v0.3.0 and above also containe an autoplugin that enables Scala compile inling (Scala 2 only). This behaviour cannot be enabled on any Pekko 1.0.x module.
+v0.3.0 and above also contains an autoplugin that enables Scala compile inlining (Scala 2 only). This behaviour cannot be enabled on any Pekko 1.0.x module.
 
 The default is:
 ```
@@ -28,4 +28,3 @@ This is usable as is in v0.2.x but in v0.3.0 and above, you need to add a file t
 * `-Dpekko.build.pekko.version=latest-release` will find latest release regardless of version number (December 2023 - this evals to `1.0.2`)
 * `-Dpekko.build.pekko.version=main` will find latest snapshot for `main` branch (December 2023 - this evals to `1.1.0-M0+...-SNAPSHOT`)
 * `-Dpekko.build.pekko.version=1.0.x` will find latest snapshot for `1.0.x` branch
-
---
+++ src/main/scala/com/github/pjfanning/pekkobuild/PekkoDevelocityPlugin.scala
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.pjfanning.pekkobuild
+
+import com.gradle.develocity.agent.sbt.DevelocityPlugin
+import com.gradle.develocity.agent.sbt.DevelocityPlugin.autoImport.{develocityConfiguration, ProjectId, Publishing}
+import sbt.{AutoPlugin, Plugins, PluginTrigger, Setting}
+
+import java.net.URI
+
+object PekkoDevelocityPlugin extends AutoPlugin {
+
+  private val ApacheDevelocityUrl   = URI.create("https://ge.apache.org").toURL
+  private val PekkoProjectId        = ProjectId("pekko")
+  private val ObfuscatedIPv4Address = "0.0.0.0"
+
+  private lazy val isCI = sys.env.get("CI").exists(_.toBoolean)
+
+  override lazy val trigger: PluginTrigger = allRequirements
+  override lazy val requires: Plugins      = DevelocityPlugin
+
+  override lazy val buildSettings: Seq[Setting[_]] = Seq(
+    develocityConfiguration := {
+      val original = develocityConfiguration.value
+      original
+        .withProjectId(PekkoProjectId)
+        .withServer(
+          original.server
+            .withUrl(Some(ApacheDevelocityUrl))
+            .withAllowUntrusted(false)
+        )
+        .withBuildScan(
+          original.buildScan
+            .withPublishing(Publishing.onlyIf(_.authenticated))
+            .withBackgroundUpload(!isCI)
+            .withObfuscation(
+              original.buildScan.obfuscation
+                .withIpAddresses(_.map(_ => ObfuscatedIPv4Address))
+            )
+        )
+    }
+  )
+}
